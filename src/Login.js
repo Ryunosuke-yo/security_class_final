0cc7210b183c99f41bbfbc4914bbdd0823acf1d4
Ryunosuke-yo <ryokokawa00@gmail.com>
2022-07-12 23:47:33 -0700

    initial

--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,7 @@ import {Link} from "@chakra-ui/react"
 
 function Login() {
 
-  const twitchUrl = `https://id.twitch.tv/oauth2/authorize?response_type=token+id_token&client_id=72msl474tph0enwp10gdlfby1sa03s&redirect_uri=http://localhost:3000/redirect&claims={"userinfo":{"picture":null,"email":null,"preferred_username":null}}&scope=openid`
+  const twitchUrl = `https://id.twitch.tv/oauth2/authorize?response_type=token+id_token&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=http://localhost:3000/redirect&claims={"userinfo":{"picture":null,"email":null,"preferred_username":null}}&scope=openid`
   return (
       <Link href={twitchUrl} isExternal>
         Login with Twitch
